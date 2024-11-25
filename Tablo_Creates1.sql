dbdd114530535c15f7f88942f911d4163b269bfe
Belinay <h.belinayy@gmail.com>
2024-11-25 16:06:43 +0300

    Çeviri

--- Tablo_Creates1.sql
+++ Tablo_Creates1.sql
@@ -1,8 +1,75 @@
 CREATE DATABASE Yurt
 
+
+
+
+---Belinayin kisim
+CREATE TABLE DormitoryInfo(
+	DormID smallint PRIMARY KEY,
+	DormName varchar(50),
+	Address varchar(200),
+	Phone char(10)
+		CONSTRAINT ck_phone 
+		check(Phone like '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
+	BlokNum tinyint,
+	DormCapacity smallint,
+	DormWorker smallint
+)
+
+CREATE TABLE Parent(
+	ParentID smallint PRIMARY KEY,
+	NameLastName varchar(50),
+	Phone char(10)
+		CONSTRAINT ck_phone 
+		check(Phone like '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
+	StudentID smallint,
+	FOREIGN KEY (StudentID) REFERENCES StudentTable (StudentID)
+)
+
+--Hilalin kisim
+CREATE TABLE MealTable (
+    MealID INT PRIMARY KEY,        -- Benzersiz yemek kimliði
+    Breakfast NVARCHAR(81),      -- Kahvaltý menüsü
+    Dinner NVARCHAR(81),         -- Akþam yemeði menüsü
+    Price DECIMAL(3, 3)          -- Yemek fiyatý
+);
+
+
+CREATE TABLE DormitoryStaff (
+    StaffID INT PRIMARY KEY,         -- Benzersiz personel kimliði
+    FullName NVARCHAR(81),          -- Personelin adý ve soyadý
+    JobTitle NVARCHAR(81),          -- Görevi
+    WorkingHours NVARCHAR(50),       -- Çalýþma saatleri
+    PhoneNumber NVARCHAR(15),        -- Telefon numarasý
+    DormID smallint,                      -- Ýlgili yurt ID'si (foreign key)
+    FOREIGN KEY (DormID) REFERENCES DormitoryInfo(DormID) -- YurtTablosu ile iliþkilendirme
+);
+
+--Zehra'nın Kısım
+CREATE TABLE DormBlockTable(
+    DormBlockID INT PRIMARY KEY,
+    BlockName CHAR(1) NOT NULL CHECK (BlockName IN ('A', 'B')),
+    RoomCount INT NOT NULL CHECK (RoomCount BETWEEN 0 AND 300),
+    Capacity INT NOT NULL CHECK (Capacity BETWEEN 0 AND 1200),
+    AvailableBeds INT NOT NULL CHECK (AvailableBeds >= 0 AND AvailableBeds <= Capacity),
+	DormID smallint,
+	FOREIGN KEY (DormID) REFERENCES DormitoryInfo(DormID), 
+);
+
+CREATE TABLE RoomTable (
+RoomID INT PRIMARY KEY,
+RoomNumber INT IDENTITY(1,1),
+RoomBlock CHAR(1) NOT NULL CHECK (RoomBlock IN ('A', 'B')), 
+Occupancy TINYINT NOT NULL CHECK (Occupancy >= 0), 
+BedCount TINYINT NOT NULL CHECK (BedCount BETWEEN 1 AND 9),
+    CHECK (Occupancy <= BedCount),
+	DormBlockID INT,
+	FOREIGN KEY (DormBlockID) REFERENCES DormBlockTable(DormBlockID), 
+);
+
 ---Haticenin kisim
 CREATE TABLE StudentTable (
-    StudentID INT IDENTITY(1000,1) PRIMARY KEY, 
+    StudentID smallint IDENTITY(1000,1) PRIMARY KEY, 
     FullName NVARCHAR(100),
     TC NVARCHAR(11), 
     IBAN NVARCHAR(34),
@@ -23,82 +90,20 @@ CREATE TABLE StudentTable (
 	PaymentID NVARCHAR(50),
     RegistrationDate DATE, 
 	ParentID INT,
+	DormID smallint,
     FOREIGN KEY (RoomID) REFERENCES RoomTable(RoomID), 
-    FOREIGN KEY (DormBlockID) REFERENCES DormBlockTable(DormBlockID)
-    FOREIGN KEY (DormID) REFERENCES DormTable(DormID),
+    FOREIGN KEY (DormBlockID) REFERENCES DormBlockTable(DormBlockID),
+    FOREIGN KEY (DormID) REFERENCES DormitoryInfo(DormID),
 	FOREIGN KEY (MealID) REFERENCES MealTable(MealID),
 	FOREIGN KEY (PaymentID) REFERENCES Paymenttable(PaymentID),
 	FOREIGN KEY (ParentID) REFERENCES ParentTable(ParentID)
 );
 
 
----Belinayin kisim
-CREATE TABLE YurtBilgi(
-	YurtID smallint PRIMARY KEY,
-	YurtAdi varchar(50),
-	Adres varchar(200),
-	Telefon char(10)
-		CONSTRAINT ck_telefon 
-		check(Telefon like '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
-	BlokSayi tinyint,
-	YurtKapasite smallint,
-	YurtCalisan smallint
-)
-
-CREATE TABLE Veli(
-	VeliID smallint PRIMARY KEY,
-	AdSoyad varchar(50),
-	Telefon char(10)
-		CONSTRAINT ck_telefon 
-		check(Telefon like '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
-	ÖðrenciID smallint
-	FOREIGN KEY (ÖðreciID) REFERENCES Öðrenci (ÖðrenciID)
-)
-
---Hilalin kisim
-CREATE TABLE MealTable (
-    MealID INT PRIMARY KEY,        -- Benzersiz yemek kimliði
-    Breakfast NVARCHAR(81),      -- Kahvaltý menüsü
-    Dinner NVARCHAR(81),         -- Akþam yemeði menüsü
-    Price DECIMAL(3, 3)          -- Yemek fiyatý
-);
-
-
-CREATE TABLE DormitoryStaff (
-    StaffID INT PRIMARY KEY,         -- Benzersiz personel kimliði
-    FullName NVARCHAR(81),          -- Personelin adý ve soyadý
-    JobTitle NVARCHAR(81),          -- Görevi
-    WorkingHours NVARCHAR(50),       -- Çalýþma saatleri
-    PhoneNumber NVARCHAR(15),        -- Telefon numarasý
-    DormID INT,                      -- Ýlgili yurt ID'si (foreign key)
-    FOREIGN KEY (DormID) REFERENCES DormitoryTable(DormID) -- YurtTablosu ile iliþkilendirme
-);
-
---Zehra'nın Kısım
-CREATE TABLE RoomTable (
-RoomID INT PRIMARY KEY,
-RoomNumber INT IDENTITY(1,1),
-RoomBlock CHAR(1) NOT NULL CHECK (Block IN ('A', 'B')), 
-Occupancy TINYINT NOT NULL CHECK (Occupancy >= 0), 
-BedCount TINYINT NOT NULL CHECK (BedCount BETWEEN 1 AND 9),
-    CHECK (Occupancy <= BedCount),
-	FOREIGN KEY (DormBlockID) REFERENCES DormBlockTable(DormBlockID), 
-);
-
-CREATE TABLE DormBlockTable (
-    DormBlockID INT PRIMARY KEY,
-    BlockName CHAR(1) NOT NULL CHECK (BlockName IN ('A', 'B')),
-    RoomCount INT NOT NULL CHECK (RoomCount BETWEEN 0 AND 300),
-    Capacity INT NOT NULL CHECK (Capacity BETWEEN 0 AND 1200),
-    AvailableBeds INT NOT NULL CHECK (AvailableBeds >= 0 AND AvailableBeds <= Capacity)
-	FOREIGN KEY (YurtID) REFERENCES YurtBilgi(YurtID), 
-);
-
-
 -- VERİ GİRİŞİ 
 --Zehra 
 --Yurt blok bilgileri / 2 adet blok var A ve B 
-INSERT INTO DormBlockTable (DormBlockID, BlockName, RoomCount, Capacity, AvailableBeds, DormitoryID)
+INSERT INTO DormBlockTable (DormBlockID, BlockName, RoomCount, Capacity, AvailableBeds, DormID)
 VALUES 
     (1, 'A', 180, 720, 20, 1),  -- A Blok, 720 kapasite, 20 boş yatak
     (2, 'B', 170, 680, 30, 1),  -- B Blok, 680 kapasite, 30 boş yatak
