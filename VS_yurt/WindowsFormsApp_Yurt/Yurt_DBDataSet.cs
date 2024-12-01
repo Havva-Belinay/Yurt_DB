40c3e4934525e54c45fe19decefe5a4498b7742a
Belinay <h.belinayy@gmail.com>
2024-12-01 23:12:25 +0300

    taşındı

---
+++ VS_yurt/WindowsFormsApp_Yurt/Yurt_DBDataSet.cs
@@ -0,0 +1,26 @@
+﻿namespace WindowsFormsApp_Yurt
+{
+
+
+    partial class Yurt_DBDataSet
+    {
+        partial class DormBlockTableDataTable
+        {
+        }
+
+        partial class MealTableDataTable
+        {
+        }
+
+        partial class DormitoryInfoDataTable
+        {
+        }
+    }
+}
+
+namespace WindowsFormsApp_Yurt.Yurt_DBDataSetTableAdapters {
+    
+    
+    public partial class DormitoryInfoTableAdapter {
+    }
+}
