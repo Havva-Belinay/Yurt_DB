d317d4995d3982a6c65394ab2651dba10dcd166e
Zehra Aşık <121757371+Cardiac54@users.noreply.github.com>
2024-12-08 22:08:48 +0300

    stored_procedure.sql

--- stored_procedure.sql
+++ stored_procedure.sql
@@ -15,3 +15,42 @@ BEGIN
         FullName LIKE @Ad + '%'; -- Ad ile başlayan isimleri döndürür
 END;
  --EXEC sp_SearchByName @Ad = 'Zehra';  arama şeklimiz bu
+
+CREATE PROCEDURE sp_SearchByFullName
+    @Ad NVARCHAR(50),       -- Kullanıcının gireceği ad
+    @Soyad NVARCHAR(50)     -- Kullanıcının gireceği soyad
+AS
+BEGIN
+    SELECT 
+        StudentID, 
+        FullName, 
+        TC, 
+        PhoneNumber, 
+        RoomNumber, 
+        DormID 
+    FROM 
+        StudentTable
+    WHERE 
+        FullName LIKE @Ad + ' ' + @Soyad; -- Tam ad ve soyad eşleşmesi
+END;
+
+--EXEC sp_SearchByFullName @Ad = 'Zehra', @Soyad = 'Bayraktar'; kullanım şekli
+
+CREATE PROCEDURE sp_SearchByPartialName
+    @NamePart NVARCHAR(100) -- Kullanıcının gireceği herhangi bir isim parçası
+AS
+BEGIN
+    SELECT 
+        StudentID, 
+        FullName, 
+        TC, 
+        PhoneNumber, 
+        RoomNumber, 
+        DormID 
+    FROM 
+        StudentTable
+    WHERE 
+        FullName LIKE '%' + @NamePart + '%'; -- Ad veya soyad içinde geçenleri döndürür
+END;
+
+--EXEC sp_SearchByPartialName @NamePart = 'ehra'; kullanım şekli
