4a4911f874a030a116c1d16a408fe6badf7f8d57
Hatice Nur Çakır <haticenurcakr@gmail.com>
2024-12-15 13:59:38 +0300

    exele veri aktarma

---
+++ WindowsFormsApp_adminPage_2/Form1.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace WindowsFormsApp_adminPage_2
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnExportToExcel;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnExportToExcel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(760, 370);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnExportToExcel
+            // 
+            this.btnExportToExcel.Location = new System.Drawing.Point(12, 400);
+            this.btnExportToExcel.Name = "btnExportToExcel";
+            this.btnExportToExcel.Size = new System.Drawing.Size(120, 30);
+            this.btnExportToExcel.TabIndex = 1;
+            this.btnExportToExcel.Text = "Excel'e Aktar";
+            this.btnExportToExcel.UseVisualStyleBackColor = true;
+            this.btnExportToExcel.Click += new System.EventHandler(this.btnExportToExcel_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.btnExportToExcel);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Excel ile Veritabanı Senkronizasyonu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ WindowsFormsApp_adminPage_2/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace WindowsFormsApp_adminPage_2
+{
+    public partial class Form1 : Form
+    {
+        // Bağlantı dizesi (örnek)
+        private string connectionString = "Server=HATICENUR;Database=yurt;Integrated Security=True;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadDataFromDatabase();
+        }
+
+        private void LoadDataFromDatabase()
+        {
+            // SQL sorgusunu yazalım
+            string query = "SELECT * FROM Worker"; // Veritabanınızdaki tablo adıyla değiştirin
+
+            // SqlConnection ve SqlDataAdapter ile veri çekelim
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(query, conn);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+
+                // DataGridView'e veri aktaralım
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        private void btnExportToExcel_Click(object sender, EventArgs e)
+        {
+            // Excel uygulamasını başlat
+            var excelApp = new Excel.Application();
+            excelApp.Visible = true;
+
+            // Yeni bir çalışma kitabı oluştur
+            Excel.Workbook workBook = excelApp.Workbooks.Add();
+            Excel.Worksheet workSheet = (Excel.Worksheet)workBook.Sheets[1];
+
+            // DataGridView başlıklarını aktar
+            for (int i = 0; i < dataGridView1.Columns.Count; i++)
+            {
+                workSheet.Cells[1, i + 1] = dataGridView1.Columns[i].HeaderText;
+            }
+
+            // DataGridView verilerini aktar
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    // Null değerler kontrol ediliyor
+                    workSheet.Cells[i + 2, j + 1] = dataGridView1.Rows[i].Cells[j].Value?.ToString();
+                }
+            }
+
+            // Çalışma kitabını kaydetmeden sadece göster
+            // workBook.SaveAs("C:\\path\\to\\your\\file.xlsx"); // Dosyayı kaydetmek isterseniz
+        }
+    }
+}
