6b2ce851f630b737e78874a3b72aaeae796b8e8e
Hatice Nur Çakır <haticenurcakr@gmail.com>
2024-12-15 15:39:22 +0300

    Add files via upload

---
+++ WindowsFormsApp_adminPage_3/WindowsFormsApp_adminPage_3/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace YedeklemeApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label labelBackupDate;
+        private System.Windows.Forms.Label labelBackupFile;
+        private System.Windows.Forms.Button buttonBackup;
+        private System.Windows.Forms.Button buttonRestore;
+        private System.Windows.Forms.DateTimePicker dateTimePickerBackup;
+        private System.Windows.Forms.TextBox textBoxBackupFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.labelBackupDate = new System.Windows.Forms.Label();
+            this.labelBackupFile = new System.Windows.Forms.Label();
+            this.buttonBackup = new System.Windows.Forms.Button();
+            this.buttonRestore = new System.Windows.Forms.Button();
+            this.dateTimePickerBackup = new System.Windows.Forms.DateTimePicker();
+            this.textBoxBackupFile = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+
+            // labelBackupDate
+            this.labelBackupDate.AutoSize = true;
+            this.labelBackupDate.Location = new System.Drawing.Point(10, 20);
+            this.labelBackupDate.Name = "labelBackupDate";
+            this.labelBackupDate.Size = new System.Drawing.Size(85, 13);
+            this.labelBackupDate.TabIndex = 0;
+            this.labelBackupDate.Text = "Yedekleme Tarihi:";
+
+            // labelBackupFile
+            this.labelBackupFile.AutoSize = true;
+            this.labelBackupFile.Location = new System.Drawing.Point(10, 60);
+            this.labelBackupFile.Name = "labelBackupFile";
+            this.labelBackupFile.Size = new System.Drawing.Size(95, 13);
+            this.labelBackupFile.TabIndex = 1;
+            this.labelBackupFile.Text = "Yedekleme Dosyası:";
+
+            // buttonBackup
+            this.buttonBackup.Location = new System.Drawing.Point(10, 100);
+            this.buttonBackup.Name = "buttonBackup";
+            this.buttonBackup.Size = new System.Drawing.Size(75, 23);
+            this.buttonBackup.TabIndex = 2;
+            this.buttonBackup.Text = "Yedekle";
+            this.buttonBackup.UseVisualStyleBackColor = true;
+            this.buttonBackup.Click += new System.EventHandler(this.buttonBackup_Click);
+
+            // buttonRestore
+            this.buttonRestore.Location = new System.Drawing.Point(100, 100);
+            this.buttonRestore.Name = "buttonRestore";
+            this.buttonRestore.Size = new System.Drawing.Size(75, 23);
+            this.buttonRestore.TabIndex = 3;
+            this.buttonRestore.Text = "Geri Yükle";
+            this.buttonRestore.UseVisualStyleBackColor = true;
+            this.buttonRestore.Click += new System.EventHandler(this.buttonRestore_Click);
+
+            // dateTimePickerBackup
+            this.dateTimePickerBackup.Location = new System.Drawing.Point(120, 20);
+            this.dateTimePickerBackup.Name = "dateTimePickerBackup";
+            this.dateTimePickerBackup.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerBackup.TabIndex = 4;
+
+            // textBoxBackupFile
+            this.textBoxBackupFile.Location = new System.Drawing.Point(120, 60);
+            this.textBoxBackupFile.Name = "textBoxBackupFile";
+            this.textBoxBackupFile.Size = new System.Drawing.Size(200, 20);
+            this.textBoxBackupFile.TabIndex = 5;
+
+            // openFileDialog
+            this.openFileDialog.Filter = "Yedekleme Dosyaları|*.bak";
+
+            // Form1
+            this.ClientSize = new System.Drawing.Size(350, 150);
+            this.Controls.Add(this.textBoxBackupFile);
+            this.Controls.Add(this.dateTimePickerBackup);
+            this.Controls.Add(this.buttonRestore);
+            this.Controls.Add(this.buttonBackup);
+            this.Controls.Add(this.labelBackupFile);
+            this.Controls.Add(this.labelBackupDate);
+            this.Name = "Form1";
+            this.Text = "Yedekleme ve Geri Yükleme";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ WindowsFormsApp_adminPage_3/WindowsFormsApp_adminPage_3/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace YedeklemeApp
+{
+    public partial class Form1 : Form
+    {
+        // SQL Server bağlantı dizesi
+        private string connectionString = @"Data Source=HATICENUR\hatic;Initial Catalog=Yurt;Integrated Security=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Yedekleme butonuna tıklanıldığında çalışacak fonksiyon
+        private void buttonBackup_Click(object sender, EventArgs e)
+        {
+            // Yedekleme dosyasının adını ve yolunu oluştur
+            string backupFilePath = @"C:\Backup\" + "Backup_" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".bak";
+
+            // Yedekleme dosyasının yolunu textbox'a yaz
+            textBoxBackupFile.Text = backupFilePath;
+
+            // SQL yedekleme sorgusu
+            string backupQuery = $"BACKUP DATABASE Yurt TO DISK = '{backupFilePath}'";
+
+            try
+            {
+                // SQL bağlantısını aç
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(backupQuery, conn);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show($"Yedekleme başarılı! Dosya yolu: {backupFilePath}", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Hata mesajı göster
+                MessageBox.Show($"Yedekleme sırasında hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Geri yükleme butonuna tıklanıldığında çalışacak fonksiyon
+        private void buttonRestore_Click(object sender, EventArgs e)
+        {
+            // Yedekleme dosyasını seçmek için OpenFileDialog
+            openFileDialog.Filter = "Yedekleme Dosyaları (*.bak)|*.bak";
+            openFileDialog.InitialDirectory = @"C:\Backup\";  // Başlangıç klasörü
+            openFileDialog.FileName = "Backup_";  // Dosya adı
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string restoreFilePath = openFileDialog.FileName;
+
+                // Seçilen dosya yolunu textBox'a yaz
+                textBoxBackupFile.Text = restoreFilePath;
+
+                string restoreQuery = $"RESTORE DATABASE Yurt FROM DISK = '{restoreFilePath}'";
+
+                try
+                {
+                    // SQL bağlantısını aç
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        SqlCommand cmd = new SqlCommand(restoreQuery, conn);
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show($"Veritabanı başarıyla geri yüklendi! Dosya yolu: {restoreFilePath}", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Hata mesajı göster
+                    MessageBox.Show($"Veritabanı geri yükleme sırasında hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                // Dosya seçilmediğinde bilgi mesajı
+                MessageBox.Show("Geri yüklemek için bir dosya seçmediniz.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
